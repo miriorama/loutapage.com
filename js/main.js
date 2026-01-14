528f2ae5fd9c2bf2f81a479dfe1fd75ac04eac28
Daniele Caraglio <daniele.caraglio@gmail.com>
2026-01-14 09:14:41 +0100

    impl: 2026

--- js/main.js
+++ js/main.js
@@ -2,95 +2,12 @@ let UI = (function() {
     let ui = {};
 
     ui.init = function() {
-        /*const cursor = document.querySelector('.cursor');
-
-        document.addEventListener('mousemove', e => {
-            requestAnimationFrame(function() {
-                cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;");
-              });
-        })
-
-        document.addEventListener('click', () => {
-            cursor.classList.add("expand");
-
-            setTimeout(() => {
-                cursor.classList.remove("expand");
-            }, 500)
-        })*/
-
-        // set the starting position of the cursor outside of the screen
-        //t clientX = -100;
-        //t clientY = -100;
-        //nst innerCursor = document.querySelector(".cursor");
-
-        //// add listener to track the current mouse position
-        //document.addEventListener("mousemove", e => {
-        //  clientX = e.clientX;
-        //  clientY = e.clientY;
-        //});
-        //
-        //// transform the innerCursor to the current mouse position
-        //// use requestAnimationFrame() for smooth performance
-        //const render = () => {
-        //  innerCursor.style.transform = `translate(${clientX}px, ${clientY}px)`;
-        //  // if you are already using TweenMax in your project, you might as well
-        //  // use TweenMax.set() instead
-        //  // TweenMax.set(innerCursor, {
-        //  //   x: clientX,
-        //  //   y: clientY
-        //  // });
-        //
-        //  requestAnimationFrame(render);
-        //};
-        //requestAnimationFrame(render);
-
-
-        //// add event listeners to all items
-        //const linkItems = document.querySelectorAll(".block");
-        //linkItems.forEach(item => {
-        //  item.addEventListener("mouseenter", function() {
-        //      innerCursor.classList.toggle('cursor-hover');
-        //  });
-        //  item.addEventListener("mouseleave", function() {
-        //      innerCursor.classList.toggle('cursor-hover');
-        //  });
-        //});
-
     }
 
     ui.scrollToEl = function(e, t) {
         t = t || 0,
         element = document.querySelector(e),
         window.scrollTo(0, element.offsetTop - t)
-    }
-
-    ui.toggleAudio = function() {
-        let $video = document.querySelector('video');
-        let $off = document.querySelector('.volume-off');
-        let $on = document.querySelector('.volume-on');
-
-        if($video.muted) {
-            $off.setAttribute('hidden', '');
-            $on.removeAttribute('hidden');
-
-            $video.muted = false;
-        } else {
-            $on.setAttribute('hidden', '');
-            $off.removeAttribute('hidden');
-
-            $video.muted = true;
-        }
-    }
-
-    ui.pauseVideo = function() {
-        //let $video = document.querySelector('video');
-        //$video.paused? $video.play() : $video.pause();
-    }
-
-    ui.showConcertsOld = function() {
-        let $concertsOld = document.querySelector('.concerts-old');
-
-        $concertsOld.classList.toggle('visible');
     }
 
     return ui;
